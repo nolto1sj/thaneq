0e0200e2d6583a92f5a90f368e44266f11581f5c
nolto1sj <sjnolton@gmail.com>
2025-10-14 23:17:55 -0400

    added base image landing page

---
+++ components/nav.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+
+export default function TopNav() {
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="fixed">
+        <Toolbar>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Thane Q
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+}
---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import TopNav from '../components/nav'
+import Image from 'mui-image'
+import Box from '@mui/material/Box';
+
+function App() {
+  return(
+  <>
+      <TopNav></TopNav>
+        <img src="../src/assets/thaneqconceptscaled.jpg" class="center"></img>
+  </>
+  )
+}
+
+export default App
